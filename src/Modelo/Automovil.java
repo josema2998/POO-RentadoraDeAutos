c4b68d32a2300a366ece9cc1c9604eeb394b4374
Armando Valdivieso Peralta <you@example.com>
2018-11-24 19:20:03 -0600

    Agregando Automovil.java

--- src/Modelo/Automovil.java
+++ src/Modelo/Automovil.java
@@ -7,8 +7,82 @@ package Modelo;
 
 /**
  *
- * @author Jose Manuel Patron
+ * @author arman
  */
 public class Automovil {
+    
+    private String Nombre;
+    private String Marca;
+    private String Placa;
+    private String Tipo;
+    private int Modelo;
+    private int Puertas;
+    private boolean Disponibilidad;
+    private double Precio;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public String getPlaca() {
+        return Placa;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public int getModelo() {
+        return Modelo;
+    }
+
+    public int getPuertas() {
+        return Puertas;
+    }
+
+    public boolean isDisponibilidad() {
+        return Disponibilidad;
+    }
+
+    public double getPrecio() {
+        return Precio;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    public void setPlaca(String Placa) {
+        this.Placa = Placa;
+    }
+
+    public void setTipo(String Tipo) {
+        this.Tipo = Tipo;
+    }
+
+    public void setModelo(int Modelo) {
+        this.Modelo = Modelo;
+    }
+
+    public void setPuertas(int Puertas) {
+        this.Puertas = Puertas;
+    }
+
+    public void setDisponibilidad(boolean Disponibilidad) {
+        this.Disponibilidad = Disponibilidad;
+    }
+
+    public void setPrecio(double Precio) {
+        this.Precio = Precio;
+    }
+    
     
 }
